df4117b54e5c35821a8f704adb07546c84fa6d5d
Marcos Vinicius <marcosprogramador123@gmail.com>
2022-09-14 20:18:53 -0300

    primeiro commit

---
+++ PycharmProjects/nascimento/script.py
@@ -0,0 +1,5 @@
+dia = input('qual o dia do seu nascimento?');
+mes = input('qual o mes do seu nascimento?');
+ano = input('qual o ano do seu nascimento?');
+
+print('você nasceu no dia', dia, 'de', mes, 'de', ano);
---
+++ PycharmProjects/soma/script.py
@@ -0,0 +1,6 @@
+
+n1 = input('digite um numero');
+n2 = input('digite outro numero');
+soma = n1 + n2;
+
+print('a soma entre', n1, 'e', n2, 'é', soma );
